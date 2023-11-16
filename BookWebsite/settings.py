a1b6c0b9157f6fd14e127de2894ae1287e7cba44
huyxoan <huyxoan1234@gmail.com>
2023-11-16 16:18:38 +0700

    update

--- BookWebsite/settings.py
+++ BookWebsite/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-&$50d$a%bi003us+yd2ms(%b6mln#kv@&lajs#mj-nb(9v!pix
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
