20b97d94176c5ef0264e7dbf45faf9b36eb6dad4
huyxoan <huyxoan1234@gmail.com>
2024-01-04 17:09:18 +0700

    official-update

--- User/views.py
+++ User/views.py
@@ -33,8 +33,12 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Dropout  
 
 import nltk
+import pickle
 nltk.download('punkt')
 nltk.download('wordnet')
+
+
+from tensorflow.keras.models import load_model 
 
 def about(request):
     context = {}
@@ -354,7 +358,6 @@ def ChatBot(request):
  
 def Chat(request):
    query = str(request.GET.get("query"))
-   # print(query)
    lemmatizer = WordNetLemmatizer()
 
    with open('jsonfile.json') as fileobj:
@@ -374,26 +377,11 @@ def Chat(request):
          if intent['tag'] not in classes:
                classes.append(intent['tag'])
 
-   # vectorizer = TfidfVectorizer()
-   # X = vectorizer.fit_transform(cleaned_word)
-
    words = sorted(set(words))
    classes = sorted(set(classes))
-
-   def clean_corpus(words):
-      words = [doc.lower() for doc in words]
-      for w in words:
-         if w not in stop_words and w.isalpha():
-               w=lemmatizer.lemmatize(w)
-               cleaned_word.append(w)
-      return(cleaned_word)
-
-#    cleaned_list = clean_corpus(words)
-
-   words = sorted(set(words))
-   classes = sorted(set(classes))
-   # y = encoder.fit_transfrom(np.array(classes), reshape(-1, 1))
-
+   pickle.dump(words, open('words.pkl','wb'))
+   pickle.dump(classes,open('classes.pkl','wb'))
+   
    training =[]
    output_empty = [0] * len(classes)
 
@@ -410,7 +398,7 @@ def Chat(request):
       output_row[classes.index(document[1])] = 1 
       training.append([bag,output_row])
 
-   '''Neural Network'''
+
    random.shuffle(training)
    training = np.array(training,dtype=object)
    train_x = list(training[:,0])
@@ -420,11 +408,14 @@ def Chat(request):
    initial_learning_rate = 0.01
    lr_schedule = tensorflow.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate, decay_steps=10000, decay_rate=0.9)
    sgd = tensorflow.keras.optimizers.SGD(learning_rate=lr_schedule, momentum=0.9, nesterov=True)
-   model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+   model.compile(loss='categorical_crossentropy',optimizer= sgd,metrics=['accuracy'])
    history = model.fit(np.array(train_x),np.array(train_y),epochs=20,batch_size=1,verbose=7)
-   # model.save('chatbot_model.model',history)
+   model.save('chatbot_model.model',history)
 
-
+   words = pickle.load(open('words.pkl','rb'))
+   classes = pickle.load(open('classes.pkl','rb'))
+   model = load_model('chatbot_model.model')
+   
    def cleaning_up_message(message):
       message_word = word_tokenize(message)
       message_word = [lemmatizer.lemmatize(word.casefold()) for word in message_word]
@@ -459,9 +450,6 @@ def Chat(request):
          if i['tag'] == tag :
                result= random.choice (i['responses'])
                break
-         # elif i['tag']!=tag:
-         #     result=random.choice(["Invalid response","Sorry:), I didn't get what you are saying","Pardon me..!"])
-         #     break
       return result
 
    print(" Aapi is Running ! ")
